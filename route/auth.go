63e48d7b2a053d16271981b78680f674a6926e63
Thiago Oliveira <thiago_ogt@outlook.com>
2018-05-11 03:08:47 -0300

    Added AuthorizeApplication middleware

--- route/auth.go
+++ route/auth.go
@@ -9,12 +9,28 @@ import (
 
 func AuthorizeWebhook(next httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		reqToken := r.Header.Get("X-Gitlab-Token")
-		if reqToken == "" || reqToken != config.Get().Gitlab.Token {
-			log.Printf("Denied webhook request on endpoint %s from host %s", r.URL, r.RemoteAddr)
-			w.WriteHeader(http.StatusUnauthorized)
-		} else {
+		if authorizeHeader(w, r, "X-Gitlab-Token", config.Get().Gitlab.Token) {
 			next(w, r, p)
 		}
+	}
+}
+
+func AuthorizeApplication(next httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		if authorizeHeader(w, r, "Authorization", config.Get().Secret) {
+			next(w, r, p)
+		}
+	}
+}
+
+func authorizeHeader(w http.ResponseWriter, r *http.Request, token, header string) (bool) {
+	auth := r.Header.Get(header)
+	if auth == "" || auth != token {
+		log.Printf("Denied request on endpoint %s from host %s", r.URL, r.RemoteAddr)
+		w.WriteHeader(http.StatusUnauthorized)
+
+		return false
+	} else {
+		return true
 	}
 }
